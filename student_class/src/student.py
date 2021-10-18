eb4856235f58ef8dcb3ac0b41704968773aa76de
Jean-Pierre Gazian <jeanpierregazian@msn.com>
2021-10-18 14:15:07 +0100

    Committing Student - Part A

--- student_class/src/student.py
+++ student_class/src/student.py
@@ -1,1 +1,10 @@
+class Student:
+    def __init__(self,input_name,input_cohort):
+        self.name = input_name
+        self.cohort = input_cohort
+    
+    def talk(self):
+        return "I can talk!"
 
+    def say_favourite_language(self,fave_language):
+        return f'I love {fave_language}'
